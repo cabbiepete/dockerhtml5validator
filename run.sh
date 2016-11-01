60f5463151283ca0c18773a84fadc2c6b4dffa14
Peter Simmons <cabbiepete@gmail.com>
2016-11-01 13:59:50 +1300

    Update run.sh
    to expose the validator website on port 8083 rather than 8080

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 # expose container port 80 (original W3C validator engine) and port 8888 (Nu Html Checker)
 docker run --detach \
-	--publish 8080:80 \
+	--publish 8083:80 \
 	--publish 8888:8888 \
 	magnetikonline/html5validator
